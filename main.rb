83074691eddceef03eaebbd93b385a9541fbc511
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-10-14 20:49:44 +0100

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,27 @@
+dial_book = {
+  "newyork" => "212",
+  "newbronswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sansfranciso" => "301",
+  "miami" => "305",
+  "palaoto" => "650",
+  "evansto" => "847",
+  "orlando" => "407"
+  "lancaster" => "717"
+}
+
+# Get city names from the hash
+def get_city_names(somehash)
+# Write code here
+end
+
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+# Write code here
+end
+
+# Execution flow
+loop do
+  # Write code here
+end
